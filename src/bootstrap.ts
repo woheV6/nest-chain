7765bb3045c74658d1cb6af3ae3e2496a56fe702
roylin <1002591652@qq.com>
2019-02-28 20:52:46 +0800

    feat: add nest

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,5 +1,8 @@
-async function bootstrap() {
-    console.log('bootstrap');
-}
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+}
 bootstrap();
